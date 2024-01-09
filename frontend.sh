a11e6f820d64a55a77086838edb86817448a5008
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-09 16:44:05 +0530

    domain conf

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,27 @@
+log_file=/tmp/expense.log
 echo insatlling nginx
-dnf install nginx -y >> /tmp/expense.log
+dnf install nginx -y >> $log_file
 
 echo copy the expence file
-cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
 
 echo removing default  fronend code
-rm -rf /usr/share/nginx/html/* >> /tmp/expense.log
+rm -rf /usr/share/nginx/html/* >> $log_file
 
 echo download frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> $log_file
 
 # shellcheck disable=SC2164
-cd /usr/share/nginx/html >> /tmp/expense.log
+cd /usr/share/nginx/html >> $log_file
 
 echo unzip frontend code
-unzip /tmp/frontend.zip >> /tmp/expense.log
+unzip /tmp/frontend.zip >> $log_file
 
 echo enable nginx
-systemctl enable nginx >> /tmp/expense.log
+systemctl enable nginx >> $log_file
 
 echo start nginx
-systemctl start nginx >> /tmp/expense.log
+systemctl start nginx >> $log_file
 
 echo restart nginx
-systemctl restart nginx >> /tmp/expense.log
+systemctl restart nginx >> $log_file
