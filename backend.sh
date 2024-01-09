74e7a9ee0194e069807418ed6de445ae2179080e
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-09 10:13:52 +0530

    this is new project

---
+++ backend.sh
@@ -0,0 +1,25 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+cp.backend.service /etc/systemd/system/backend.service
+
+useradd expense
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+cd /app
+npm install
+
+
+
+systemctl daemon-reload
+
+systemctl enable backend
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
