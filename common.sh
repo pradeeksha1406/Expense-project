670018ef3cfee0a125c715f23931f969704aeb56
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-10 14:22:28 +0530

    component decleration

--- common.sh
+++ common.sh
@@ -1,1 +1,10 @@
 log_file=/tmp/expense.log
+
+download_and_extract()
+{
+echo download $component code
+curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
+
+echo unzip $component code
+unzip /tmp/$component.zip >> $log_file
+}
--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,6 @@
 source common.sh
+component=frontend
+
 echo insatlling nginx
 dnf install nginx -y >> $log_file
 
@@ -8,14 +10,11 @@ cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
 echo removing default  fronend code
 rm -rf /usr/share/nginx/html/* >> $log_file
 
-echo download frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> $log_file
-
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html >> $log_file
 
-echo unzip frontend code
-unzip /tmp/frontend.zip >> $log_file
+download_and_extract
+
 
 echo enable nginx
 systemctl enable nginx >> $log_file
