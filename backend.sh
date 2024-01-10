14c4e8233de5d36b25a6c46ac7d755e572b57000
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-10 14:59:27 +0530

    backend function declare

--- backend.sh
+++ backend.sh
@@ -1,28 +1,46 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+source common.sh
 
+echo disable existing nodejs
+dnf module disable nodejs -y >> $log_file
+
+echo enable nodejs
+dnf module enable nodejs:18 -y >> $log_file
+
+echo installing nodejs
 dnf install nodejs -y
-cp backend.service /etc/systemd/system/backend.service
 
-useradd expense
+echo copy bakend service
+cp backend.service /etc/systemd/system/backend.service >> $log_file
 
-mkdir /app
+ehco useradd
+useradd expense >> $log_file
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+echo making diretory
+mkdir /app >> $log_file
 
-cd /app
+echo downloading the bakend code
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >> $log_file
 
-unzip /tmp/backend.zip
+cd /app >> $log_file
 
-npm install
+echo extracting the backed code zip
+unzip /tmp/backend.zip >> $log_file
+
+echo relove code dependecieys
+npm install >> $log_file
 
 
+echo system reload after edit in backen config file
+systemctl daemon-reload >> $log_file
 
-systemctl daemon-reload
+echo backend
+systemctl enable backend >> $log_file
 
-systemctl enable backend
-systemctl start backend
+echo bakend start
+systemctl start backend >> $log_file
 
-dnf install mysql -y
+echo installing mysql client
+dnf install mysql -y >> $log_file
 
+echo load the schema
 mysql -h mysql.techadda.co -uroot -pExpenseApp@1 < /app/schema/backend.sql
