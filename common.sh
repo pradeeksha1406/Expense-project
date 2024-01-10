5e7b2eeb6d18d7111de240e1bcf48df09247693b
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-10 15:09:09 +0530

    backend function declare

--- common.sh
+++ common.sh
@@ -9,10 +9,8 @@ echo unzip $component code
 unzip /tmp/$component.zip >> $log_file
 }
 download_and_extract1(){
-  echo downloading the backend code
-  curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >> $log_file
-  downlad
-  echo extracting the backed code zip
-  unzip /tmp/backend.zip >> $log_file
-
+echo downloading the backend code
+curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >> $log_file
+echo extracting the backed code zip
+unzip /tmp/backend.zip >> $log_file
 }
