78cbbc4d3c89a7e961668fbd5119eb0f77cf27b6
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-10 16:34:52 +0530

    AaA

--- Mysql.sh
+++ Mysql.sh
@@ -1,19 +1,19 @@
 source common.sh
 
 echo disable old version of sql
-dnf module disable mysql -y >> $log_file
+dnf module disable mysql -y &&>>$log_file
 
 echo copy repo file
-cp mysql.repo /etc/yum.repos.d/mysql.repo >> $log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &&>>$log_file
 
 echo installing mysql
-dnf install mysql-community-server -y >> $log_file
+dnf install mysql-community-server -y &&>>$log_file
 
 echo enable mysql
-systemctl enable mysqld >> $log_file
+systemctl enable mysqld &&>>$log_file
 
 echo start mysql
-systemctl start mysqld >> $log_file
+systemctl start mysqld &&>>$log_file
 
 echo set root pass
-mysql_secure_installation --set-root-pass ExpenseApp@1 >> $log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &&>>$log_file
--- backend.sh
+++ backend.sh
@@ -2,43 +2,43 @@ source common.sh
 
 
 echo disable existing nodejs
-dnf module disable nodejs -y >> $log_file
+dnf module disable nodejs -y &>>$log_file
 
 echo enable nodejs
-dnf module enable nodejs:18 -y >> $log_file
+dnf module enable nodejs:18 -y &>>$log_file
 
 echo installing nodejs
 dnf install nodejs -y
 
 echo copy bakend service
-cp backend.service /etc/systemd/system/backend.service >> $log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 ehco useradd
-useradd expense >> $log_file
+useradd expense &>>$log_file
 
 echo making diretory
-mkdir /app >> $log_file
+mkdir /app &>>$log_file
 
 echo moving to app directory
-cd /app >> $log_file
+cd /app &>>$log_file
 
 download_and_extract1
 
 echo relove code dependecieys
-npm install >> $log_file
+npm install &>>$log_file
 
 
 echo system reload after edit in backen config file
-systemctl daemon-reload >> $log_file
+systemctl daemon-reload &>>$log_file
 
 echo backend
-systemctl enable backend >> $log_file
+systemctl enable backend &>>$log_file
 
 echo bakend start
-systemctl start backend >> $log_file
+systemctl start backend &>>$log_file
 
 echo installing mysql client
-dnf install mysql -y >> $log_file
+dnf install mysql -y &>>$log_file
 
 echo load the schema
 mysql -h mysql.techadda.co -uroot -pExpenseApp@1 < /app/schema/backend.sql
