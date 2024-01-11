f3ebc7549d760d166698863b2da9ab3cb6635e62
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-11 12:00:12 +0530

    if else condition

--- frontend.sh
+++ frontend.sh
@@ -5,25 +5,26 @@ echo insatlling nginx
 dnf install nginx -y &>>$log_file
 # shellcheck disable=SC1073
 if [ $? -eq 0 ]; then
-  echo success
-  else
-    echo failed
-    fi 
+  if [ $? -eq 0 ]; then
+    echo -e "\e[31mSuccess\e[0m"
+    else
+    echo -e "\e[31mFailed\e[0m"
+      fi
 
 echo copy the expence file
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[31mSuccess\e[0m"
   else
-    echo failed
+  echo -e "\e[31mFailed\e[0m"
     fi 
 
 echo removing default  fronend code
 rm -rf /usr/share/nginx/html/* &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[31mSuccess\e[0m"
   else
-    echo failed
+  echo -e "\e[31mFailed\e[0m"
     fi 
 
 # shellcheck disable=SC2164
@@ -36,23 +37,23 @@ download_and_extract
 echo enable nginx
 systemctl enable nginx &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[31mSuccess\e[0m"
   else
-    echo failed
+  echo -e "\e[31mFailed\e[0m"
     fi 
 
 echo start nginx
 systemctl start nginx &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[31mSuccess\e[0m"
   else
-    echo failed
+  echo -e "\e[31mFailed\e[0m"
     fi 
 
 echo restart nginx
 systemctl restart nginx &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[31mSuccess\e[0m"
   else
-    echo failed
+  echo -e "\e[31mFailed\e[0m"
     fi 
