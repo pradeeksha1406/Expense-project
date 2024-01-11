6ef021cd13af4f73c14208861ffb3aac949dd7d2
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-11 12:54:35 +0530

    if else condition

--- Mysql.sh
+++ Mysql.sh
@@ -2,48 +2,24 @@ source common.sh
 
 echo disable old version of sql
 dnf module disable mysql -y &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
 
 echo copy repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
 
 echo installing mysql
 dnf install mysql-community-server -y &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
 
 echo enable mysql
 systemctl enable mysqld &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
 
 echo start mysql
 systemctl start mysqld &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
 
 echo set root pass
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi 
+status_check
--- backend.sh
+++ backend.sh
@@ -3,107 +3,55 @@ source common.sh
 
 echo disable existing nodejs
 dnf module disable nodejs -y &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo enable nodejs
 dnf module enable nodejs:18 -y &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo installing nodejs
 dnf install nodejs -y
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo copy bakend service
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 ehco useradd
 useradd expense &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo making diretory
 mkdir /app &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo moving to app directory
 cd /app &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 download_and_extract1
 
 echo relove code dependecieys
 npm install &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 
 echo system reload after edit in backen config file
 systemctl daemon-reload &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo backend
 systemctl enable backend &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo bakend start
 systemctl start backend &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo installing mysql client
 dnf install mysql -y &>>$log_file
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
 
 echo load the schema
 mysql -h mysql.techadda.co -uroot -pExpenseApp@1 < /app/schema/backend.sql
-if [$? -eq 0];then
-  echo success
-  else
-    echo failed
-    fi
+status_check
