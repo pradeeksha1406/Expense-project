9f8e7b9532e18fa300ae3923243af253184b44e3
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-11 14:38:11 +0530

    if else condition

--- common.sh
+++ common.sh
@@ -4,12 +4,12 @@ download_and_extract()
 {
 echo download $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
-echo $?
+status_check
 
 echo unzip $component code
 unzip /tmp/$component.zip >> $log_file
 
-echo $?
+status_check
 }
 download_and_extract1()
 {
